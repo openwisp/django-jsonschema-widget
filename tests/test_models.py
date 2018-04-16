4d6a92119141998e4a26dff85f3ffd48eb3224c0
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-04-16 22:29:47 +0600

    test files

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-jsonschema-widget
+------------
+
+Tests for `django-jsonschema-widget` models module.
+"""
+
+from django.test import TestCase
+
+from jsonschema_widget import models
+
+
+class TestJsonschema_widget(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
